a8009f23434c5709637148472345437309252ddb
Rafael Albuquerque <rafael.albuquerque.it@gmail.com>
2021-06-01 20:52:34 -0300

    Delete Client

--- backend/src/main/java/com/albuquerque/dsCatalogClients/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/albuquerque/dsCatalogClients/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.albuquerque.dsCatalogClients.services.exceptions.DatabaseException;
 import com.albuquerque.dsCatalogClients.services.exceptions.ResourceNotFoundException;
 
 
@@ -27,5 +28,16 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 	}
 	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
 }
 
