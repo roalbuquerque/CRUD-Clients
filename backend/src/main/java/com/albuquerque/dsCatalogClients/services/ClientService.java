307a40400325dcd935e60a522fd6e38e5fdbf57d
Rafael Albuquerque <rafael.albuquerque.it@gmail.com>
2021-06-01 20:29:05 -0300

    POST new Client

--- backend/src/main/java/com/albuquerque/dsCatalogClients/services/ClientService.java
+++ backend/src/main/java/com/albuquerque/dsCatalogClients/services/ClientService.java
@@ -32,4 +32,16 @@ public class ClientService {
 		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
 }
