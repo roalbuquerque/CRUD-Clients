ee667bdd70ce1bd1958751ec1236e743fdd7202c
Rafael Albuquerque <rafael.albuquerque.it@gmail.com>
2021-06-01 20:38:37 -0300

    Update Client

--- backend/src/main/java/com/albuquerque/dsCatalogClients/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/albuquerque/dsCatalogClients/resources/exceptions/ResourceExceptionHandler.java
@@ -9,14 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.albuquerque.dsCatalogClients.services.exceptions.EntityNotFoundException;
+import com.albuquerque.dsCatalogClients.services.exceptions.ResourceNotFoundException;
 
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 	
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
--- backend/src/main/java/com/albuquerque/dsCatalogClients/services/ClientService.java
+++ backend/src/main/java/com/albuquerque/dsCatalogClients/services/ClientService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.albuquerque.dsCatalogClients.dto.ClientDTO;
 import com.albuquerque.dsCatalogClients.entities.Client;
 import com.albuquerque.dsCatalogClients.repositories.ClientRepository;
-import com.albuquerque.dsCatalogClients.services.exceptions.EntityNotFoundException;
+import com.albuquerque.dsCatalogClients.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -29,7 +31,7 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
 
@@ -43,5 +45,18 @@ public class ClientService {
 		entity.setChildren(dto.getChildren());
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		}
+		catch(EntityNotFoundException e){
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 	}
 }
