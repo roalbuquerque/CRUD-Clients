b85ba27b9f7858df5c685bc1791dbbe9b4c92bd6
Rafael Albuquerque <rafael.albuquerque.it@gmail.com>
2021-06-02 16:43:36 -0300

    Pagination

--- backend/src/main/java/com/albuquerque/dsCatalogClients/services/ClientService.java
+++ backend/src/main/java/com/albuquerque/dsCatalogClients/services/ClientService.java
@@ -1,14 +1,14 @@
 package com.albuquerque.dsCatalogClients.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,9 +26,9 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<ClientDTO> findAll(){
-		List<Client> list = repository.findAll();
-		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	public Page<ClientDTO> findAllPaged(PageRequest pagRequest){
+		Page<Client> list = repository.findAll(pagRequest);
+		return list.map(x -> new ClientDTO(x));
 	}
 
 	@Transactional(readOnly = true)
